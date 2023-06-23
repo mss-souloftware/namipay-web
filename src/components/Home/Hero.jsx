8fd7bd184ca224bdd0d2be9e6b908b89f5dace9f
mss-souloftware <sufyan.souloftware@gmail.com>
2023-06-23 12:22:36 +0500

    hero structure

--- src/components/Home/Hero.jsx
+++ src/components/Home/Hero.jsx
@@ -8,10 +8,10 @@ export default function Hero() {
             <HeroBg />
             <section className="relative h-screen">
                 <h1 className="text-white text-[10vw] font-extralight uppercase">The Future</h1>
-                <h2 className="text-white text-[10vw] font-extralight uppercase text-center -m-12">OF <span className="mainTitleGradient font-medium leading-10">SAUDI</span></h2>
-                <div className="relative w-[100%] h-full block">
-                    <h2 className="mainTitleGradient absolute text-white text-[10vw] uppercase font-medium -m-12  left-0 ">PAYMENT</h2>
-                    <p className="text-white text-3xl absolute ">
+                <h2 className="text-white text-[10vw] font-extralight uppercase text-center -mt-6">OF <span className="mainTitleGradient font-medium">SAUDI</span></h2>
+                <div className="relative w-[100%] flex items-center justify-between">
+                    <h2 className="mainTitleGradient translate-x-[84%] w-2/4 text-white text-[10vw] uppercase font-medium -mt-6">PAYMENT</h2>
+                    <p className="text-white text-2xl w-2/4 pl-10 opacity-0">
                         Leveraging the latest in financial technologies, Nami offers its partners state-of-the-art payment processing solutions, that are tailored to meet business needs.
                     </p>
                 </div>
--- src/components/Home/HeroBg.jsx
+++ src/components/Home/HeroBg.jsx
@@ -1,8 +1,19 @@
+import Image from "next/image";
 
 export default function HeroBg() {
   return (
-    <div className="heroBgFixed h-screen w-screen fixed top-0 left-0 bg-themeGreen -z-10 bg-[url('/images/banners/home-hero-bg.jpg')] bg-cover bg-no-repeat">
-
+    <div className="heroBgFixed overflow-hidden flex items-center justify-center h-screen w-screen fixed top-0 left-0 bg-themeGreen -z-10 bg-[url('/images/banners/home-hero-bg.jpg')] bg-cover bg-no-repeat">
+      <div className="h-[120vh] w-[120vw] relative top-0 left-0 text-center flex items-center justify-center z-10">
+        <div className="absolute top-0 bottom-0 m-auto h-[full] w-full">
+          <Image src='/images/banners/animeBg/circle-dots.png' layout='fill' objectFit='contain' />
+        </div>
+        <div className="absolute top-0 bottom-0 m-auto h-[98%] w-[98%]">
+          <Image src='/images/banners/animeBg/circle-solid.png' layout='fill' objectFit='contain' />
+        </div>
+        <div className="absolute top-0 bottom-0 m-auto h-[75%] w-[75%]">
+          <Image src='/images/banners/animeBg/circle-dots.png' layout='fill' objectFit='contain' />
+        </div>
+      </div>
     </div>
   )
 }
