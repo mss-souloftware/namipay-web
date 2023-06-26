42bb351a9865f3a9133faff33e7dbb1cb6c26239
mss-souloftware <sufyan.souloftware@gmail.com>
2023-06-26 14:28:47 +0500

    hero complete desktop

--- src/components/Home/Hero.jsx
+++ src/components/Home/Hero.jsx
@@ -2,6 +2,7 @@ import React, { useRef, useLayoutEffect } from "react";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 import styles from './Hero.module.css';
+import Link from "next/link";
 
 
 export default function Hero() {
@@ -9,11 +10,14 @@ export default function Hero() {
     const textRef = useRef(null);
     const mainRef = useRef(null);
     const peraReveal = useRef(null);
+    const heroBtnFloat = useRef(null);
 
     useLayoutEffect(() => {
         const textElement = textRef.current;
         const mainElement = mainRef.current;
         const peraRevealElement = peraReveal.current;
+        const heroBtnFloatElement = heroBtnFloat.current;
+
         gsap.registerPlugin(ScrollTrigger);
 
         const tl = gsap.timeline({
@@ -22,6 +26,7 @@ export default function Hero() {
                 start: "75% 80%",
                 end: "70% 80%",
                 scrub: 3,
+                // markers: true,
             },
         });
 
@@ -30,7 +35,7 @@ export default function Hero() {
             {
                 x: 0,
                 y: 0,
-                duration: 3,
+                duration: 4,
                 ease: "power1.inOut",
             },
             "a"
@@ -39,8 +44,17 @@ export default function Hero() {
             {
                 y: 0,
                 opacity: 1,
-                duration: 3,
+                duration: 4,
                 ease: "power1.inOut",
+            },
+            "a"
+        ).to(
+            heroBtnFloatElement,
+            {
+                y: 0,
+                opacity: 1,
+                duration: 4,
+                ease: "power1.in",
             },
             "a"
         );
@@ -49,23 +63,30 @@ export default function Hero() {
 
     return (
         <section ref={mainRef} className={styles.heroAnimeStart}>
-            <div className="relative h-screen">
+            <div className={`${styles.headingsWraper} relative h-screen`}>
                 <h1 className="text-white font-extralight uppercase">
                     The Future
                 </h1>
                 <h2 className="text-white font-extralight uppercase text-center -mt-6">
-                    OF <span className="mainTitleGradient font-medium">SAUDI</span>
+                    OF <span className={`${styles.mainTitleGradient} font-medium`}>SAUDI</span>
                 </h2>
                 <div className="relative w-[100%] flex items-center justify-between">
                     <h2
                         ref={textRef}
-                        className="mainTitleGradient translate-x-[84%] w-2/4 text-white uppercase font-medium -mt-6"
+                        className={`${styles.mainTitleGradient} translate-x-[84%] w-2/4 text-white uppercase font-medium -mt-6`}
                     >
                         PAYMENT
                     </h2>
                     <p ref={peraReveal} className="text-white text-2xl font-light w-2/4 pl-10 opacity-0 translate-y-[45%] peraReveal">
                         Leveraging the latest in financial technologies, Nami offers its partners state-of-the-art payment processing solutions, that are tailored to meet business needs.
                     </p>
+                </div>
+                <div ref={heroBtnFloat} className="relative w-[100%] text-center mt-10 flex items-center justify-between opacity-0 translate-y-5 heroBtnFloat">
+                    <Link className="whiteToGreen m-auto" href='/'>Contact Us
+                        <div className="circles">
+                            <span></span>
+                        </div>
+                    </Link>
                 </div>
             </div>
         </section>
